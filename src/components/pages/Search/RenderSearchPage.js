a09b45acefae95fbbfe99b277ea7b9c4fbddebf4
Jonah Aitchison <jonah@pop-os.localdomain>
2020-07-14 12:54:30 -0500

    initial search

--- src/components/pages/Search/RenderSearchPage.js
+++ src/components/pages/Search/RenderSearchPage.js
@@ -1,56 +1,75 @@
 import React from "react";
 import PropTypes from "prop-types";
 
+import {
+  Flex,
+  Text,
+  Box,
+  Image,
+  Select,
+  Input,
+  Divider
+} from "@chakra-ui/core";
+
+import { FaRegHeart, FaRegStar, FaSortDown, FaStar } from "react-icons/fa";
+
 const RenderSearchPage = props => (
   <div>
     {props.data.map(book => (
-      <div key={book.id} className="bookContainer">
-        <div className="bookThumbStatus">
-          <div className="thumbnail">
-            {/* Thumbnail */}
-            <img
+      <>
+        <Flex key={book.id} w="440px">
+          <Box roundedTopLeft="lg" overflow="hidden">
+            <Image
               src={book.volumeInfo.imageLinks.smallThumbnail}
               alt={book.volumeInfo.title}
+              pb="0"
+              mb="0"
             />
-          </div>
-          <div className="status">
-            {/* status dropdown */}
-            dropdown
-          </div>
-        </div>
-        <div className="bookDetails">
-          <div className="bookTitleFavIcon">
-            <div className="bookTitleAuthor">
-              {/* Book Title and Author */}
-              <a href={book.selfLink}>{book.volumeInfo.title}</a>
-              <div>{book.volumeInfo.authors}</div>
-            </div>
-            <div className="FavIcon">
-              {/* Favorite Icon */}
-              :)
-            </div>
-          </div>
-          <div>
-            <div className="bookRating">
-              {/* Star Rating */}
-              {book.volumeInfo.averageRating}
-            </div>
-
-            {/* 
-						<div>
-							<div>
-								Date Started
-								<input type="text" />
-							</div>
-							<div>
-								Date Finshed
-								<input type="text" />
-							</div>
-						</div>
-						*/}
-          </div>
-        </div>
-      </div>
+            <Select
+              size="sm"
+              icon={FaSortDown}
+              rounded="0"
+              roundedBottomLeft="lg"
+              fontSize=".875rem"
+              pt="0"
+              mt="0"
+            >
+              <option value="1">To Read</option>
+              <option value="2">Reading</option>
+              <option value="3">Finished</option>
+            </Select>
+          </Box>
+          <Box pl="1rem">
+            <Flex alignItems="start">
+              <Box>
+                <Text fontSize="1rem" fontFamily="Frank Ruhl Libre">
+                  {book.volumeInfo.title}
+                </Text>
+                <Text fontSize="0.875rem" fontFamily="Frank Ruhl Libre">
+                  {book.volumeInfo.authors[0]}
+                </Text>
+              </Box>
+              <Box>
+                <Box as={FaRegHeart} size="1.5rem" color="rgb(210, 71, 25)" />
+              </Box>
+            </Flex>
+            <Box>
+              <Box>
+                <Box as={FaStar} size="1.25rem" color="#E8E8E8" pr=".125rem" />
+                <Box as={FaStar} size="1.25rem" color="#E8E8E8" pr=".125rem" />
+                <Box as={FaStar} size="1.25rem" color="#E8E8E8" pr=".125rem" />
+                <Box as={FaStar} size="1.25rem" color="#E8E8E8" pr=".125rem" />
+                <Box as={FaStar} size="1.25rem" color="#E8E8E8" pr=".125rem" />
+              </Box>
+              {/* <Flex direction="row">
+						<Box><Input placeholder="Date Started" size="sm" w="75%" borderColor="rgb(232,232,232)" /></Box>
+						<Box><Input placeholder="Date Finished" size="sm" w="75%" borderColor="rgb(232,232,232)" /></Box>
+					</Flex> */}
+            </Box>
+          </Box>
+        </Flex>
+        <Divider />
+      </>
     ))}
   </div>
 );
